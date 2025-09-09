4250f43e2786655004917e84bb61a7a1067f33b3
catsonkeyboard <leemeany@gmail.com>
2025-09-09 18:36:27 +0800

    feat:first commit

---
+++ src/stack.rs
@@ -0,0 +1,64 @@
+
+#[derive(Debug)]
+struct Stack<T> {
+    size: usize,
+    data: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack {
+            size: 0,
+            data: Vec::new()
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+
+    fn len(&self) -> usize {
+        self.size
+    }
+
+    fn clear(&mut self) {
+        self.data.clear();
+        self.size = 0;
+    }
+
+    fn push(&mut self, value: T) {
+        self.data.push(value);
+        self.size += 1;
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.is_empty() {
+            None
+        } else {
+            self.size -= 1;
+            self.data.pop()
+        }
+    }
+
+    fn peek(&self) -> Option<&T> {
+        if self.is_empty() {
+            None
+        } else {
+            self.data.get(self.size - 1)
+        }
+    }
+
+    fn peek_mut(&mut self) -> Option<&mut T> {
+        if self.is_empty() {
+            None
+        } else {
+            self.data.get_mut(self.size - 1)
+        }
+    }
+
+
+
+
+
+
+}
