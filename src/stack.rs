fbcb58c5e0b4639783c72813ec03030880b7ad3b
catsonkeyboard <leemeany@gmail.com>
2025-09-10 12:56:17 +0800

    feat:update Stack

--- src/stack.rs
+++ src/stack.rs
@@ -5,6 +5,41 @@ struct Stack<T> {
     data: Vec<T>,
 }
 
+struct IntoIter<T>(Stack<T>);
+impl<T: Clone> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.0.is_empty() {
+            self.0.size -= 1;
+            self.0.data.pop()
+        } else{
+            None
+        }
+    }
+}
+
+struct Iter<'a, T: 'a> {
+    stack: Vec<&'a T>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
+
+struct IterMut<'a, T: 'a> {
+    stack: Vec<&'a mut T>
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
+
 impl<T> Stack<T> {
     fn new() -> Self {
         Stack {
@@ -56,9 +91,32 @@ impl<T> Stack<T> {
         }
     }
 
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 
+    fn iter(&self) -> Iter<T> {
+        let mut iterator = Iter { stack: Vec::new() };
+        for item in self.data.iter() {
+            iterator.stack.push(item);
+        }
+        iterator
+    }
+}
 
+#[test]
+fn test_stack_basic() {
+    let mut s = Stack::new();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    println!("size:{}, {:?}", s.size, s);
 
-
-
+    let pop = s.pop();
+    println!("pop: {:?}", pop);
+    println!("size:{}, {:?}", s.size, s);
+    while let Some(pop) = s.pop() {
+        println!("pop: {:?}", pop);
+        println!("size:{}, {:?}", s.size, s);
+    }
 }
